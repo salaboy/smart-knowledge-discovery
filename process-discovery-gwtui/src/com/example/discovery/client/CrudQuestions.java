27e072110dbbbf00936c250789fef7647783f8a3
Leandro-Romero <leandro.s.romero@gmail.com>
2011-01-21 16:02:45 -0300

    UI initial commit. It might have some problems with maven and gwt.

---
+++ process-discovery-gwtui/src/com/example/discovery/client/CrudQuestions.java
@@ -0,0 +1,85 @@
+package com.example.discovery.client;
+
+
+import java.util.LinkedList;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class CrudQuestions extends HorizontalPanel {
+
+	private VerticalPanel verticalPanel = new VerticalPanel();
+	private Label questionLabel = new Label("Question:");
+	private TextBox textBoxQuestion = new TextBox(); 
+	private Label descriptionLabel = new Label("Description:");
+	private TextArea description = new TextArea();
+	private Button addQuestionButton = new Button("Add question");
+	private Button backButton = new Button("Back");
+	
+	public CrudQuestions() {
+		
+		verticalPanel.add(questionLabel);
+		verticalPanel.add(textBoxQuestion);
+		verticalPanel.add(descriptionLabel);
+		verticalPanel.add(description);
+		verticalPanel.add(addQuestionButton);
+		verticalPanel.add(backButton);
+		
+		add(verticalPanel);
+		
+		LinkedList<String> categories = new LinkedList<String>();//TODO: Hardcodeado
+		//Hacer peticion al core por las categorias.
+		categories.add("Easy");
+		categories.add("Difficult");
+		
+		CheckBoxList checkBoxList = new CheckBoxList("Categories", categories);
+		
+		add(checkBoxList);
+
+		//TODO: Nueva clase que encapsule el backButton?
+		backButton.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				RootPanel.get().clear();
+				RootPanel.get().add(new MainPanel());
+			}
+		});
+		
+		addQuestionButton.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				//TODO: Mandar peticion de agregar una pregunta.
+				
+				final PopupPanel popUp = new PopupPanel(true);
+				VerticalPanel popUpContent = new VerticalPanel();
+				
+				Label label = new Label("The question was added.");
+				Button closeButton = new Button("Close");
+				
+				popUpContent.add(label);
+				popUpContent.add(closeButton);
+				
+				popUp.add(popUpContent);
+				popUp.center();
+				
+				closeButton.addClickHandler(new ClickHandler() {
+					
+					@Override
+					public void onClick(ClickEvent event) {
+						popUp.hide();
+					}
+				});
+			}
+		});
+	}
+}
---
+++ process-discovery-gwtui/src/com/example/discovery/client/CuestionaryPanel.java
@@ -0,0 +1,59 @@
+package com.example.discovery.client;
+
+import java.util.LinkedList;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class CuestionaryPanel extends HorizontalPanel {
+	
+	private VerticalPanel verticalPanel = new VerticalPanel();
+	private Button createCuestionaryButton = new Button("Create Cuestionary");
+	private Button backButton = new Button("Back");
+	private FlexTable questionTable = new FlexTable();
+	
+	public CuestionaryPanel() {
+	
+		questionTable.setText(0, 0, "Select");
+		questionTable.setText(0, 1, "Question");
+		questionTable.setText(0, 2, "Description");
+		
+		verticalPanel.add(questionTable);
+		verticalPanel.add(createCuestionaryButton);
+		verticalPanel.add(backButton);
+		
+		backButton.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				RootPanel.get().clear();
+				RootPanel.get().add(new MainPanel());
+			}
+		});
+		
+		questionTable.getRowFormatter().addStyleName(0, "listHeader");
+		questionTable.addStyleName("list");
+		
+		add(verticalPanel);
+		
+		LinkedList<String> categories = new LinkedList<String>();//TODO: Hardcodeado
+		categories.add("Easy");
+		categories.add("Difficult");
+		add(new CheckBoxList("Categories", categories));
+		
+		addQuestion();
+	}
+	
+	private void addQuestion() {
+		
+		questionTable.setWidget(1, 0, new CheckBox());
+		questionTable.setText(1, 1, "Ugis o La Continental?");
+		questionTable.setText(1, 2, "Pregunta decisiva.");
+	}
+}
