fe824ac4afe261a0b8940bc84b4ce83f00b2ce7f
Leandro Romero <leandro.s.romero@gmail.com>
2011-02-01 19:01:22 -0300

    Some minor changes to support SmartGwt.

--- process-discovery-gwtui/src/main/java/com/worpdress/salaboy/smartprocessdiscovery/client/SmartProcessDiscovery.java
+++ process-discovery-gwtui/src/main/java/com/worpdress/salaboy/smartprocessdiscovery/client/SmartProcessDiscovery.java
@@ -2,13 +2,27 @@ package com.worpdress.salaboy.smartprocessdiscovery.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.smartgwt.client.util.SC;
+import com.smartgwt.client.widgets.IButton;
+import com.smartgwt.client.widgets.events.ClickEvent;
+import com.smartgwt.client.widgets.events.ClickHandler;
 
-public class SmartProcessDiscovery implements EntryPoint {
-
-	
-	public void onModuleLoad() {
-	
+public class SmartProcessDiscovery implements EntryPoint
+{
+	public void onModuleLoad()
+	{
 		RootPanel.get("topMenu").add(new TopMenu());
 		RootPanel.get().add(new CrudQuestions());
+		
+		IButton button = new IButton("Hello world");
+		button.addClickHandler(new ClickHandler()
+		{
+			public void onClick(ClickEvent event)
+			{
+				SC.say("Hello World from SmartGWT");
+			}
+		});
+		
+		button.draw();
 	}
 }
