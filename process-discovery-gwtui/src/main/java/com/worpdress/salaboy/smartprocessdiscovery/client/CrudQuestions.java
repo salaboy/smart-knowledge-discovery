ad00f8cf9a8b7dc6ffee058c6881d2e1b690bee6
Leandro Romero <leandro.s.romero@gmail.com>
2011-02-03 23:58:11 -0300

    Refactor. CategoryList class created.

--- process-discovery-gwtui/src/main/java/com/worpdress/salaboy/smartprocessdiscovery/client/CrudQuestions.java
+++ process-discovery-gwtui/src/main/java/com/worpdress/salaboy/smartprocessdiscovery/client/CrudQuestions.java
@@ -1,19 +1,12 @@
 package com.worpdress.salaboy.smartprocessdiscovery.client;
 
-import com.smartgwt.client.data.AdvancedCriteria;
 import com.smartgwt.client.data.DataSource;
-import com.smartgwt.client.data.fields.DataSourceTextField;
-import com.smartgwt.client.types.OperatorId;
-import com.smartgwt.client.types.SelectionAppearance;
 import com.smartgwt.client.types.SelectionStyle;
 import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.IButton;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.grid.ListGrid;
-import com.smartgwt.client.widgets.grid.ListGridRecord;
-import com.smartgwt.client.widgets.grid.events.SelectionChangedHandler;
-import com.smartgwt.client.widgets.grid.events.SelectionEvent;
 import com.smartgwt.client.widgets.layout.HLayout;
 import com.smartgwt.client.widgets.layout.VLayout;
 
@@ -21,7 +14,7 @@ public class CrudQuestions extends HLayout {
 
 	private VLayout crudLayout = new VLayout();
 	private ListGrid questionList = new ListGrid();	
-	private ListGrid categoryList = new ListGrid();
+	private CategoryList categoryList = new CategoryList(questionList);
 	
 	private HLayout buttonLayout = new HLayout();
 	private IButton addQuestionButton = new IButton("Add Question");
@@ -77,41 +70,7 @@ public class CrudQuestions extends HLayout {
 		
 		crudLayout.setMembers(questionList, buttonLayout);
 		crudLayout.setMembersMargin(10);
-		
-		DataSource categoryListDataSource = new DataSource();
-	    DataSourceTextField categoryNameField = new DataSourceTextField("itemCategoryName", "Filter Categories", 100, true);
-	    categoryNameField.setPrimaryKey(true);
-	    categoryListDataSource.addField(categoryNameField);
-	    categoryListDataSource.setClientOnly(true);
-	    categoryListDataSource.setTestData(new CategoryListRecord[] {new CategoryListRecord("Comida"), 
-	    															 new CategoryListRecord("Futbol"),
-	    															 new CategoryListRecord("IT")});
-	    
-	    categoryList.setDataSource(categoryListDataSource);
-		categoryList.setWidth(200);
-		categoryList.setHeight(100);
-		categoryList.setShowAllRecords(true);
-		categoryList.setAutoFetchData(true);
-		categoryList.setSelectionAppearance(SelectionAppearance.CHECKBOX);
-		
-		categoryList.addSelectionChangedHandler(new SelectionChangedHandler() {
-
-			public void onSelectionChanged(SelectionEvent event) {
 				
-				ListGridRecord[] categories = categoryList.getSelection();
-				
-				String[] filterCategories = new String[categories.length];
-				
-				for(int i = 0; i < categories.length; i++) {
-					filterCategories[i] = categories[i].getAttribute("itemCategoryName");
-				}
-				
-				AdvancedCriteria criteria = new AdvancedCriteria("itemCategory", OperatorId.NOT_IN_SET, filterCategories);
-				
-				questionList.filterData(criteria);
-			}
-		});
-		
 		addMember(crudLayout);
 		addMember(categoryList);
 	}
