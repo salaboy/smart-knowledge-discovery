90984e1aa2fb9fe2c192555b5d8422ed17fe7e1c
Leandro Romero <leandro.s.romero@gmail.com>
2011-02-10 11:23:11 -0300

    First approach to rest support.

---
+++ knowledge-discovery-rest/src/main/java/com/wordpress/salaboy/smartknowledgediscovery/QuestionsResource.java
@@ -0,0 +1,105 @@
+package com.wordpress.salaboy.smartknowledgediscovery;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.URI;
+import java.util.LinkedList;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+ 
+@Path("question")
+public class QuestionsResource {
+    
+	//TODO: Implement persistence.
+	static private LinkedList<Question> questionList = new LinkedList<Question>();
+	
+	@POST
+    @Consumes("application/xml")    
+    @Path("add")
+    public Response addQuestion(InputStream questionData) throws IOException, ParserConfigurationException, SAXException {
+        
+		Question question = buildQuestion(questionData);
+		questionList.add(question);
+		
+		return Response.created(URI.create("/" + (questionList.size() - 1))).build();
+    }
+	
+	//TODO:  Use JAXB to parse XML or JSON instead of XML.  
+	//TODO: Modify to support real questions and SmartGwt.
+	private Question buildQuestion(InputStream questionData) throws ParserConfigurationException, SAXException, IOException {
+
+		Question question = new Question();
+
+		DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+		Document document = documentBuilder.parse(questionData);
+		document.getDocumentElement().normalize();
+
+		NodeList nodeList = document.getElementsByTagName("question");
+		Node customerRoot = nodeList.item(0);
+
+		if (customerRoot.getNodeType() == Node.ELEMENT_NODE) {
+
+			Element element = (Element)customerRoot;
+			NodeList childNodes = element.getChildNodes();
+
+			for (int i = 0; i < childNodes.getLength(); i++) {
+
+				Element childElement = (Element)childNodes.item(i);
+				String tagName = childElement.getTagName();
+				System.out.println(tagName);
+				String textContent = childElement.getTextContent();
+				System.out.println(textContent);
+
+				if (tagName.equals("text")) {
+					question.setText(textContent);
+				} else if (tagName.equals("description")) {
+					question.setDescription(textContent);
+				}            
+			}
+		} 
+
+		return question;
+	}
+
+	//TODO: Support for smartgwt.
+	@GET
+	@Produces("application/xml")
+	@Path("get/{id}")
+	public StreamingOutput getQuestion(@PathParam ("id") String questionId) {
+
+		final Question question = questionList.get(Integer.parseInt(questionId));
+		
+		return new StreamingOutput() {
+			public void write(OutputStream outputStream) {
+				PrintWriter out = new PrintWriter(outputStream);
+				
+				out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+							"<question>" + 
+							"<text>" + question.getText() + "</text>" + 
+							"<description>" + question.getDescription() + "</description>" +
+							"</question>");
+				out.close();
+			}
+
+		};
+	}
+}
---
+++ knowledge-discovery-rest/src/test/java/com/wordpress/salaboy/smartknowledgediscovery/QuestionResourceTest.java
@@ -0,0 +1,42 @@
+package com.wordpress.salaboy.smartknowledgediscovery;
+
+import java.io.ByteArrayInputStream;
+
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.InputStreamRequestEntity;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.RequestEntity;
+import org.junit.Test;
+
+public class QuestionResourceTest {
+
+	@Test
+	public void add() {
+		String addCustomerXML =
+	        "<question>" +
+	            "<text>Java or Python?</text>" +
+	            "<description>Not relevant.</description>" +
+	        "</question>";
+
+	    PostMethod postMethod = new PostMethod("http://localhost:8080/knowledge-discovery-rest/rest/question/add");
+	    RequestEntity entity = new InputStreamRequestEntity(new ByteArrayInputStream(addCustomerXML.getBytes()), "application/xml");
+	    postMethod.setRequestEntity(entity);
+	    HttpClient client = new HttpClient();
+	    
+	    try {
+	    	int result = client.executeMethod(postMethod);
+	    	System.out.println("Response status code: " + result);
+	    	System.out.println("Response headers:");
+	    	
+	    	Header[] headers = postMethod.getResponseHeaders();
+	    	for (int i = 0; i < headers.length; i++) {
+	    		System.out.println(headers[i].toString());
+	    	}   	
+	    }catch (Exception e) {
+	    	e.printStackTrace();
+	    } finally {
+	    	postMethod.releaseConnection();
+	    }	
+	}	
+}
