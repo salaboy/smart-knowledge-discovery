6c0aab95ae84f02547c8b3902b88ba654aaa0115
Juan Grande <juan@plugtree.com>
2011-02-23 16:57:48 -0300

    Removed an old import

--- knowledge-discovery-core/src/main/java/com/plugtree/smartprocessdiscovery/dao/impl/GenericDaoJpa.java
+++ knowledge-discovery-core/src/main/java/com/plugtree/smartprocessdiscovery/dao/impl/GenericDaoJpa.java
@@ -10,8 +10,6 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
-import com.plugtree.smartprocessdiscovery.services.EntityService;
-
 /**
  * creation date: 2/23/11
  */
