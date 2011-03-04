161338eace260c249cd8bcbc2cc85b36d9ab9d8b
nicolas-sanchez <sancheznicolas89@gmail.com>
2011-03-04 14:52:34 -0300

    InterviewRequest/Response added, test for Interview 1st approach

--- knowledge-discovery-rest/src/main/java/com/plugtree/smartknowledgediscovery/InterviewResource.java
+++ knowledge-discovery-rest/src/main/java/com/plugtree/smartknowledgediscovery/InterviewResource.java
@@ -21,160 +21,159 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
 
+import com.plugtree.smartknowledgediscovery.util.InterviewRequest;
+import com.plugtree.smartknowledgediscovery.util.InterviewResponse;
+import com.plugtree.smartknowledgediscovery.util.QuestionResponse;
 import com.plugtree.smartprocessdiscovery.model.process.Interview;
+import com.plugtree.smartprocessdiscovery.model.questionaire.Answer;
+import com.plugtree.smartprocessdiscovery.model.questionaire.AnsweredQuestionnaire;
+import com.plugtree.smartprocessdiscovery.model.questionaire.Question;
+import com.plugtree.smartprocessdiscovery.model.questionaire.Questionnaire;
 import com.plugtree.smartprocessdiscovery.services.InterviewService;
 import com.plugtree.smartprocessdiscovery.services.ServiceException;
 
 @Path("/interview")
 public class InterviewResource {
 
-    private InterviewService interviewService;
-    private static String EOL = "\n";
+	private InterviewService interviewService;
 
-    @POST
-    @Consumes("application/xml")
-    @Path("/add")
-    public Response addInterview(InputStream interviewData) throws IOException, ParserConfigurationException,
-            SAXException {    	
-        String description = obtainDescription(interviewData);
-        long questionnaireId = obtainQuestionnaireId(interviewData);
-        long personId = obtainPersonId(interviewData);
-        try {
-            Long interviewId = interviewService.create(description, questionnaireId, personId);
-            return Response.created(URI.create("/" + (interviewId - 1))).build();
-        } catch (ServiceException e) {
-            // TODO: add error message into response        	
-        	e.printStackTrace();        		
-        	return Response.created(URI.create("/error")).status(Status.INTERNAL_SERVER_ERROR).build();
-        
-        }
-    }
+	@POST
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	@Path("/get")
+	public InterviewResponse getInterview(InterviewRequest interviewRequest) throws ServiceException{
 
-    private long obtainPersonId(InputStream interviewData) {
-        // TODO: parse input to obtain the person id
-        return 0;
-    }
+		InterviewResponse interviewResponse = new InterviewResponse();
 
-    private long obtainQuestionnaireId(InputStream interviewData) {
-        // TODO: parse input to obtain the questionnaire id
-        return 0;
-    }
+		for (Interview interview : interviewService.findAll()) {
+			interviewResponse.addInterview(interview);
+		}
 
-    private String obtainDescription(InputStream interviewData) {
-        // TODO: parse input to obtain the description
-        return null;
-    }
+		interviewResponse.setStatus(InterviewResponse.STATUS_SUCCESS);
 
-    @GET
-    @Produces("application/xml")
-    @Path("/get/{id}")
-    public StreamingOutput getInterview(@PathParam("id") final String interviewId) {
+		return interviewResponse;
+	}
 
-        final Interview interview;
-        try {
-            interview = interviewService.get(Long.parseLong(interviewId));
-        } catch (final ServiceException e) {
-            return new StreamingOutput() {
-                public void write(OutputStream outputStream) {
-                    PrintWriter out = new PrintWriter(outputStream);
-                    out.append("<error>" + EOL);
-                    out.append("  <message> " + e.getMessage() + "</message>" + EOL);
-                    out.append("<error>" + EOL);
-                    out.close();
-                }
+	@POST
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	@Path("/add")
+	public InterviewResponse addInterview(InterviewRequest interviewRequest) throws ServiceException{
 
-            };
-        }
+		for (Interview interview : interviewRequest.getInterviews()) {
+			interviewService.create(interview.getDescription(),interview.getQuestionnaire().getId(),interview.getPerson().getId());
+		}
+		
+		System.out.println(interviewService.findAll().size());
+		
+		InterviewResponse interviewResponse = new InterviewResponse();
+		interviewResponse.setStatus(InterviewResponse.STATUS_SUCCESS);
 
-        return new StreamingOutput() {
-            public void write(OutputStream outputStream) {
-                PrintWriter out = new PrintWriter(outputStream);
-                out.append("<interview>" + EOL);
-                out.append("  <id>" + interview.getId() + "</id>" + EOL);
-                out.append("  <description>" + interview.getDescription() + "</description>" + EOL);
-                out.append("  <duedate>" + interview.getDueDate() + "</duedate>" + EOL);
-                out.append("  <enddate>" + interview.getEndDate() + "</enddate>" + EOL);
-                out.append("  <person>" + interview.getPerson().getId() + "</person>" + EOL);
-                out.append("  <questionnaire>" + interview.getQuestionnaire().getId() + "</questionnaire>" + EOL);
-                out.append("</interview>");
-                out.close();
-            }
-        };
-    }
+		return interviewResponse;
+	}
 
-    @POST
-    @Consumes("application/xml")
-    @Path("/update")
-    public Response updateInterview(Long id, String description, Date dueDate, Date startDate, Date endDate,
-            Long questionnaireId, Long personId) {
-        try {
-            interviewService.update(id, description, dueDate, startDate, endDate, questionnaireId, personId);
-        } catch (ServiceException e) {
-            // TODO: add error message into response
-            return Response.created(URI.create("/error")).status(Status.INTERNAL_SERVER_ERROR).build();
-        }
-        return Response.created(URI.create("/" + id)).build();
-    }
+	@POST
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	@Path("/update")
+	public InterviewResponse updateInterview(InterviewRequest interviewRequest) throws ServiceException {
 
-    @POST
-    @Consumes("application/xml")
-    @Path("/remove")
-    public Response remove(Long id) {
-        try {
-            interviewService.remove(id);
-        } catch (ServiceException e) {
-            // TODO: add error message into response
-            return Response.created(URI.create("/error")).status(Status.INTERNAL_SERVER_ERROR).build();
-        }
-        return Response.created(URI.create("/" + id)).build();
-    }
+		for (Interview interview : interviewRequest.getInterviews()) {
+			interviewService.update(interview.getId(),
+					interview.getDescription(),
+					interview.getDueDate(),
+					interview.getStartDate(),
+					interview.getEndDate(),
+					interview.getQuestionnaire().getId(),
+					interview.getPerson().getId());
+		}
 
-    @POST
-    @Consumes("application/xml")
-    @Path("/list")
-    public Response findAll(Long id, Long catId) {
-        try {
-            Collection<Interview> all = interviewService.findAll();
-            // TODO: create response message
-            return Response.created(URI.create("/" + id)).build();
-        } catch (ServiceException e) {
-            // TODO: add error message into response
-            return Response.created(URI.create("/error")).status(Status.INTERNAL_SERVER_ERROR).build();
-        }
-    }
+		InterviewResponse interviewResponse = new InterviewResponse();
+		interviewResponse.setStatus(InterviewResponse.STATUS_SUCCESS);
 
-    @POST
-    @Consumes("application/xml")
-    @Path("/answer/add")
-    public Response addAnswer(Long id, Long questionId, String text) {
-        try {
-            interviewService.addAnswer(id, questionId, text);
-        } catch (ServiceException e) {
-            // TODO: add error message into response
-            return Response.created(URI.create("/error")).status(Status.INTERNAL_SERVER_ERROR).build();
-        }
-        return Response.created(URI.create("/" + id + -1)).build();
-    }
+		return interviewResponse;
+	}
 
-    @POST
-    @Consumes("application/xml")
-    @Path("/answer/remove")
-    public Response removeAnswer(Long id, Long questionId) {
-        try {
-            interviewService.removeAnswer(id, questionId);
-        } catch (ServiceException e) {
-            // TODO: add error message into response
-            return Response.created(URI.create("/error")).status(Status.INTERNAL_SERVER_ERROR).build();
-        }
-        return Response.created(URI.create("/" + id + -1)).build();
-    }
+	@POST
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	@Path("/remove")
+	public InterviewResponse remove(InterviewRequest interviewRequest) throws ServiceException {
+		for (Interview interview : interviewRequest.getInterviews()) {
+			interviewService.remove(interview.getId());
+		}
 
-    public void setInterviewService(InterviewService interviewService) {
-        this.interviewService = interviewService;
-    }
+		InterviewResponse interviewResponse = new InterviewResponse();
+		interviewResponse.setStatus(InterviewResponse.STATUS_SUCCESS);
 
-    public InterviewService getInterviewService() {
-        return interviewService;
-    }
+		return interviewResponse;		
+	}
+
+	@POST
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	@Path("/list")
+	public  InterviewResponse findAll(InterviewRequest interviewRequest) throws ServiceException {
+		InterviewResponse interviewResponse = new InterviewResponse();
+
+		for (Interview interview : interviewService.findAll()) {
+			interviewResponse.addInterview(interview);
+		}
+
+		interviewResponse.setStatus(InterviewResponse.STATUS_SUCCESS);
+
+		return interviewResponse;
+
+	}
+
+	@POST
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	@Path("/answer/add")
+	public InterviewResponse addAnswer(InterviewRequest interviewRequest) throws ServiceException {
+
+		for (Interview interview : interviewRequest.getInterviews()) {
+			AnsweredQuestionnaire ansQuest = interview.getQuestionnaire();			
+
+			for(Question question : ansQuest.getQuestionnaire().getQuestions()){
+				Answer ans = ansQuest.getAnswer(question);
+				interviewService.addAnswer(ans.getId(),ans.getQuestion().getId(), ans.getText());
+			}
+		}
+		InterviewResponse interviewResponse = new InterviewResponse();
+		interviewResponse.setStatus(InterviewResponse.STATUS_SUCCESS);
+
+		return interviewResponse;
+
+
+	}
+
+	@POST
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	@Path("/answer/remove")
+	public InterviewResponse removeAnswer(InterviewRequest interviewRequest) throws ServiceException {
+
+		for (Interview interview : interviewRequest.getInterviews()) {
+			AnsweredQuestionnaire ansQuest = interview.getQuestionnaire();			
+
+			for(Question question : ansQuest.getQuestionnaire().getQuestions()){
+				Answer ans = ansQuest.getAnswer(question);
+				interviewService.removeAnswer(ans.getId(),question.getId());
+			}
+		}
+		InterviewResponse interviewResponse = new InterviewResponse();
+		interviewResponse.setStatus(InterviewResponse.STATUS_SUCCESS);
+
+		return interviewResponse;
+	}
+
+	public void setInterviewService(InterviewService interviewService) {
+		this.interviewService = interviewService;
+	}
+
+	public InterviewService getInterviewService() {
+		return interviewService;
+	}
 
 }
--- knowledge-discovery-rest/src/main/java/com/plugtree/smartknowledgediscovery/QuestionsResource.java
+++ knowledge-discovery-rest/src/main/java/com/plugtree/smartknowledgediscovery/QuestionsResource.java
@@ -15,65 +15,81 @@ import com.plugtree.smartknowledgediscovery.util.QuestionResponse;
 import com.plugtree.smartprocessdiscovery.dao.GenericDao;
 import com.plugtree.smartprocessdiscovery.model.questionaire.Question;
 
- 
+
 @Path("/question")
 public class QuestionsResource {
-    
+
 	private GenericDao<Question> questionService;
-	
+
 	@POST
-    @Consumes("application/xml")
-    @Produces("application/xml")
-    @Path("/add")
-    public QuestionResponse addQuestion(QuestionRequest questionRequest) {
-			
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	@Path("/get")
+	public QuestionResponse getQuestion(QuestionRequest questionRequest) {
+
+		QuestionResponse questionResponse = new QuestionResponse();
+
+		for (Question question : questionService.listAll()) {
+			questionResponse.addQuestion(question);
+		}				
+		questionResponse.setStatus(QuestionResponse.STATUS_SUCCESS);
+
+		return questionResponse;
+	}
+
+	@POST
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	@Path("/add")
+	public QuestionResponse addQuestion(QuestionRequest questionRequest) {
+
 		for (Question question : questionRequest.getQuestions()) {
 			questionService.save(question);
 		}
-						
+
 		QuestionResponse questionResponse = new QuestionResponse();
 		questionResponse.setStatus(QuestionResponse.STATUS_SUCCESS);
 
 		return questionResponse;
-    }
-	
+	}
+
 	@POST
-    @Consumes("application/xml")
-    @Produces("application/xml")
-    @Path("/remove")
-    public QuestionResponse removeQuestion(QuestionRequest questionRequest){
-        							
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	@Path("/remove")
+	public QuestionResponse removeQuestion(QuestionRequest questionRequest){
+
 		for (Question question : questionRequest.getQuestions()) {
 			questionService.remove(question);;
 		}
-		
+
 		QuestionResponse questionResponse = new QuestionResponse();
 		questionResponse.setStatus(QuestionResponse.STATUS_SUCCESS);
-		
+
 		return questionResponse;
-    }
-	
+	}
+
 	@POST
-    @Consumes("application/xml")
-    @Produces("application/xml")
-    @Path("/update")
-    public QuestionResponse updateQuestion(QuestionRequest questionRequest){
-        
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	@Path("/update")
+	public QuestionResponse updateQuestion(QuestionRequest questionRequest){
+
 		for (Question question : questionRequest.getQuestions()) {
 			questionService.update(question);	
 		}
-		
+
 		QuestionResponse questionResponse = new QuestionResponse();
 		questionResponse.setStatus(QuestionResponse.STATUS_SUCCESS);
-				
+
 		return questionResponse;
-    }
+	}
 
-    public void setQuestionService(GenericDao<Question> questionService) {
-        this.questionService = questionService;
-    }
+	public void setQuestionService(GenericDao<Question> questionService) {
+		this.questionService = questionService;
+	}
 
-    public GenericDao<Question> getQuestionService() {
-        return questionService;
-    }
+	public GenericDao<Question> getQuestionService() {
+		return questionService;
+	}
 }
