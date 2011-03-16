8d10c6ed09d272bb116e888c01b38f75d93a3a06
Leandro Romero <leandro.s.romero@gmail.com>
2011-03-16 12:02:52 -0300

    QuestionServiceImpl stub.

--- knowledge-discovery-gwtui/src/main/java/com/plugtree/smartknowledgediscovery/client/QuestionService.java
+++ knowledge-discovery-gwtui/src/main/java/com/plugtree/smartknowledgediscovery/client/QuestionService.java
@@ -6,5 +6,12 @@ import com.google.gwt.user.client.rpc.RemoteService;
 import com.plugtree.smartprocessdiscovery.model.questionaire.Question;
 
 public interface QuestionService extends RemoteService {
-		List<Question> fetch();
+		
+    List<Question> fetch();
+		
+    List<Question> add(Question question);
+    
+    List<Question> remove(Long questionId);
+    
+    List<Question> update(Question question);
 }
--- knowledge-discovery-gwtui/src/main/java/com/plugtree/smartknowledgediscovery/server/QuestionServiceImpl.java
+++ knowledge-discovery-gwtui/src/main/java/com/plugtree/smartknowledgediscovery/server/QuestionServiceImpl.java
@@ -1,18 +1,56 @@
 package com.plugtree.smartknowledgediscovery.server;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-import com.plugtree.smartknowledgediscovery.client.GreetingService;
 import com.plugtree.smartknowledgediscovery.client.QuestionService;
 import com.plugtree.smartprocessdiscovery.model.questionaire.Question;
 
-public class QuestionServiceImpl extends RemoteServiceServlet implements
-QuestionService {
+@SuppressWarnings("serial")
+public class QuestionServiceImpl extends RemoteServiceServlet implements QuestionService {
 
 	public List<Question> fetch() {
-		// TODO Auto-generated method stub
-		return null;
+
+	    LinkedList<Question> list = new LinkedList<Question>();
+
+	    Question question1 = new Question("what?");
+	    Question question2 = new Question("how?");
+
+	    list.add(question1);
+	    list.add(question2);
+
+	    return list;
 	}
 
+    @Override
+    public List<Question> add(Question question) {
+
+        LinkedList<Question> list = new LinkedList<Question>();
+
+        list.add(question);
+
+        return list;
+    }
+
+    @Override
+    public List<Question> remove(Long questionId) {
+
+        LinkedList<Question> list = new LinkedList<Question>();
+        Question question = new Question("Removed?");
+
+        list.add(question);
+
+        return list;
+    }
+
+    @Override
+    public List<Question> update(Question question) {
+
+        LinkedList<Question> list = new LinkedList<Question>();
+
+        list.add(question);
+ 
+        return list;
+    }
 }
