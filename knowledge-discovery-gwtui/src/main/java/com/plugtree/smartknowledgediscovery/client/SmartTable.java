f0cb86dff719bf6291354d229f729881b7910d42
Leandro Romero <leandro.s.romero@gmail.com>
2011-03-21 14:35:22 -0300

    Add SmartTable class.

---
+++ knowledge-discovery-gwtui/src/main/java/com/plugtree/smartknowledgediscovery/client/SmartTable.java
@@ -0,0 +1,51 @@
+package com.plugtree.smartknowledgediscovery.client;
+
+import java.util.List;
+
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.Label;
+
+public class SmartTable<T> extends FlexTable {
+
+    Label tableTitle = new Label();
+    
+    public SmartTable(String title) {
+        tableTitle.setText(title);
+    }
+    
+    public void removeRecords() {
+        
+        for (int i = 2; i < getRowCount(); i++) {
+            removeRow(i);
+        }
+    }
+    
+    public void setUp(QuestionDataSource dataSource) {
+
+        setWidget(0, 0, tableTitle);
+
+        List<Field> fields = dataSource.getFields();
+
+        getFlexCellFormatter().setColSpan(0, 0, fields.size() + 2);
+
+        int columNumber = 0;
+
+        for (Field field : fields) {
+            setText(1, columNumber, field.getName());
+            columNumber++;
+        }
+
+        setText(1, columNumber, "Remove");
+        setText(1, columNumber + 1, "Edit");
+
+        addStyle();
+    }
+    
+    private void addStyle() {
+
+        getRowFormatter().addStyleName(0, "listHeader");
+        getRowFormatter().addStyleName(1, "listHeader");
+        addStyleName("list");
+        tableTitle.addStyleName("label");
+    }
+}
