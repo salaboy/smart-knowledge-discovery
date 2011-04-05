c4eec6e498f07fd78dc960be6806ec19c7e034a4
Leandro Romero <leandro.s.romero@gmail.com>
2011-04-05 09:49:21 -0300

    Bugfix in CategoryServiceImpl.

--- knowledge-discovery-gwtui/src/main/java/com/plugtree/smartknowledgediscovery/server/CategoryServiceImpl.java
+++ knowledge-discovery-gwtui/src/main/java/com/plugtree/smartknowledgediscovery/server/CategoryServiceImpl.java
@@ -1,42 +1,38 @@
 package com.plugtree.smartknowledgediscovery.server;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-import com.plugtree.smartknowledgediscovery.client.QuestionsService;
-import com.plugtree.smartprocessdiscovery.model.questionaire.Question;
+import com.plugtree.smartknowledgediscovery.client.CategoryService;
+import com.plugtree.smartprocessdiscovery.model.common.Category;
 
 @SuppressWarnings("serial")
-public class CategoryServiceImpl extends RemoteServiceServlet implements QuestionsService {
+public class CategoryServiceImpl extends RemoteServiceServlet implements CategoryService {
 
     @Override
-    public List<Question> fetch() {
-        // TODO Auto-generated method stub
-        return null;
+    public List<Category> fetch() {
+        return new LinkedList<Category>();
     }
 
     @Override
-    public List<Question> fetchWithFilter(String filter) {
-        // TODO Auto-generated method stub
-        return null;
+    public List<Category> fetchWithFilter(String filter) {
+        return new LinkedList<Category>();
     }
 
     @Override
-    public List<Question> add(Question question) {
-        // TODO Auto-generated method stub
-        return null;
+    public List<Category> add(Category category) {
+        return new LinkedList<Category>();
     }
 
     @Override
-    public List<Question> remove(Long questionId) {
-        // TODO Auto-generated method stub
-        return null;
+    public List<Category> remove(Long categoryId) {
+        return new LinkedList<Category>();
     }
 
     @Override
-    public List<Question> update(Question question) {
-        // TODO Auto-generated method stub
-        return null;
+    public List<Category> update(Category category) {
+        return new LinkedList<Category>();
     }
-
 }
+    
